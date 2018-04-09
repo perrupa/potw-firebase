0da6382811a654f8f581dbd5d57bf41fd54a227d
Chris Marlow <christopher.marlow@shopify.com>
2018-04-08 20:15:14 -0400

    adds redux dev tools

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import ReduxPromise from 'redux-promise';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers';
 import routes from './routes';
@@ -13,11 +14,15 @@ import 'bootstrap-social';
 // for bundling your styles
 import './bundle.scss';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(ReduxPromise),
+))
+
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.querySelector('.react-root')
