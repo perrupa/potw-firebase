e5871ba09141e5b0cc89dd3a209b9a59129677f3
Chris Marlow <christopher.marlow@shopify.com>
2018-04-08 22:24:44 -0400

    improving linting

--- src/app/config.js
+++ src/app/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Change this to your firebase configuration! (Add Firebase to your web app)
   FIREBASE_CONFIG: {
-    apiKey: 'AIzaSyBcTasAwIUYBPqMmqLqQRgHdjxMTN4doqE',
-    authDomain: 'radiant-inferno-2816.firebaseapp.com',
-    databaseURL: 'https://radiant-inferno-2816.firebaseio.com',
-    storageBucket: 'radiant-inferno-2816.appspot.com',
-    messagingSenderId: '1095877180097',
-  },
-}
+    apiKey: "AIzaSyBcTasAwIUYBPqMmqLqQRgHdjxMTN4doqE",
+    authDomain: "radiant-inferno-2816.firebaseapp.com",
+    databaseURL: "https://radiant-inferno-2816.firebaseio.com",
+    storageBucket: "radiant-inferno-2816.appspot.com",
+    messagingSenderId: "1095877180097"
+  }
+};
